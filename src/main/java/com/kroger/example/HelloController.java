617b16e5c90710a2d5d6880b2ec3253f07c7fbd2
Karl Greenhalgh <karl.greenhalgh@kroger.com>
2021-05-12 14:16:57 -0400

    Cleaning up java source

--- src/main/java/com/kroger/example/HelloController.java
+++ src/main/java/com/kroger/example/HelloController.java
@@ -2,6 +2,8 @@ package com.kroger.example;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletResponse;
 //import javax.servlet.http;
 
 @RestController
@@ -9,43 +11,24 @@ public class HelloController {
 	private String password="SuperSpecialAisle7";
 	private String token="TheF5KeyIsSoRefreshing";
 	private String pat_token="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";
-	String s = null;
-	
-	protected void doGet() {
-	try {
-		//doSomeWork();
-		System.out.println(s.toString());
-	} catch (NullPointerException ex) {
-		// BAD: printing a stack trace back to the response
-		ex.printStackTrace();
-		return;
-	}
-
-	/*try {
-		doSomeWork();
-	} catch (NullPointerException ex) {
-		// GOOD: log the stack trace, and send back a non-revealing response
-		log("Exception occurred", ex);
-		response.sendError(
-			HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
-			"Exception occurred");
-		return;
-	}*/
-}
 
 	@RequestMapping("/")
 	public String index() {
-		doGet();
-		/*try {
-//doSomeWork();
-} catch (NullPointerException ex) {
-// BAD: printing a stack trace back to the response
-ex.printStackTrace();
-return;
-}*/
 		return "Greetings from Spring Boot!";
 	}
-	
-	
+
+	// Should trigger: https://github.com/github/codeql/blob/main/java/ql/src/Security/CWE/CWE-209/StackTraceExposure.ql
+	// Should trigger: (don't have url) Unnecessary toString()
+	@RequestMapping("/hello")
+	public String helloWorld(HttpServletResponse response) throws Exception  {
+		try {
+			String temp = "blah";
+			System.out.println(temp.toString());
+		} catch (NullPointerException ex) {
+			ex.printStackTrace(response.getWriter());
+		}
+
+		return "Hello world";
+	}
 
 }
